971cc7581913ca4fc6867b8ad08a41f645167dcf
Ghulam Mustafa <157630245+Mustafa4561@users.noreply.github.com>
2024-01-24 18:33:19 +0000

    Create project.cpp

---
+++ project.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+class Calculator()
+{
+private:
+double num1;
+double num2;
+
+public:
+Calculator();
+double Sum(double x,double y);
+double Minus(double x,double y);
+double Multiplication(double x,double y);
+double Division(double x,double y);
+};
+
+
+
+int main()
+{
+
+return 0;
+}
